e9ce3305d6042f30de07f72719b1d3049af49313
Janine Wagner <janine.vvagner@gmail.com>
2025-02-13 20:14:01 -0700

    diable cache in prod

--- next.config.ts
+++ next.config.ts
@@ -11,13 +11,10 @@ const nextConfig: NextConfig = {
 // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
 config.experiments = { ...config.experiments, asyncWebAssembly: true }
 
-// https://nextjs.org/docs/app/building-your-application/optimizing/memory-usage#disable-webpack-cache
-// This just stops building altogether:
-// if (config.cache && !dev) {
-//     config.cache = Object.freeze({
-//         type: 'memory',
-//     })
-// }
+// Disable webpack cache in production to prevent the pack error
+if (process.env.NODE_ENV === 'production') {
+  config.cache = false
+}
 
 // Deubbing (vercel/next.js/issues/27650)
 config.infrastructureLogging = { debug: /PackFileCache/ }
